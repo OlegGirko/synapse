d2459af52e0172a4b23c72b39e85d78807ff00cc
Oleg Girko <ol@infoserver.lv>
2015-11-25 01:32:19 +0000

    Use proper paths for uploads, database, pid and log files.
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- synapse/config/server.py
+++ synapse/config/server.py
@@ -416,7 +416,7 @@ class ServerConfig(Config):
         except ValueError as e:
             raise ConfigError(str(e))
 
-        self.pid_file = self.abspath(config.get("pid_file"))
+        self.pid_file = self.runpath(config.get("pid_file"))
         self.soft_file_limit = config.get("soft_file_limit", 0)
         self.daemonize = bool(config.get("daemonize"))
         self.print_pidfile = bool(config.get("print_pidfile"))
@@ -937,7 +937,7 @@ class ServerConfig(Config):
             bind_port = 8448
             unsecure_port = 8008
 
-        pid_file = os.path.join(data_dir_path, "homeserver.pid")
+        pid_file = self.runpath("homeserver.pid")
 
         secure_listeners = []
         unsecure_listeners = []
