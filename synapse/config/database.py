4747c92702c48efeaaa3ea481f068fdec1e3ad93
Oleg Girko <ol@infoserver.lv>
2015-11-25 01:32:19 +0000

    Use proper paths for uploads, database, pid and log files.
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- synapse/config/database.py
+++ synapse/config/database.py
@@ -159,7 +159,7 @@ class DatabaseConfig(Config):
 
     def generate_config_section(self, data_dir_path, **kwargs):
         return DEFAULT_CONFIG % {
-            "database_path": os.path.join(data_dir_path, "homeserver.db")
+            "database_path": self.varpath("homeserver.db")
         }
 
     def read_arguments(self, args):
@@ -191,7 +191,7 @@ class DatabaseConfig(Config):
     def set_databasepath(self, database_path):
 
         if database_path != ":memory:":
-            database_path = self.abspath(database_path)
+            database_path = self.varpath(database_path)
 
         self.databases[0].config["args"]["database"] = database_path
 
