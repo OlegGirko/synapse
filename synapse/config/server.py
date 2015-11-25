bfae10ce312a2eb9724229bb203be64e09a95b3f
Oleg Girko <ol@infoserver.lv>
2015-11-25 01:32:19 +0000

    Use proper paths for uploads, database, pid and log files.
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- synapse/config/server.py
+++ synapse/config/server.py
@@ -258,7 +258,7 @@ class ServerConfig(Config):
         except ValueError as e:
             raise ConfigError(str(e))
 
-        self.pid_file = self.abspath(config.get("pid_file"))
+        self.pid_file = self.runpath(config.get("pid_file"))
         self.web_client_location = config.get("web_client_location", None)
         self.soft_file_limit = config.get("soft_file_limit", 0)
         self.daemonize = config.get("daemonize")
@@ -696,7 +696,7 @@ class ServerConfig(Config):
             bind_port = 8448
             unsecure_port = 8008
 
-        pid_file = os.path.join(data_dir_path, "homeserver.pid")
+        pid_file = self.runpath("homeserver.pid")
 
         # Bring DEFAULT_ROOM_VERSION into the local-scope for use in the
         # default config string
